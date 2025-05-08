9ab5ef4665bf555c71aa92687a81b2611a9949de
Vlad Buhaescu <vlad.buhaescu@qoobiss.com>
2025-05-08 17:20:00 +0300

    update package and bundle name

--- ontrace_flutter_plugin/example/android/app/build.gradle.kts
+++ ontrace_flutter_plugin/example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ontrace_flutter_plugin_example"
+    namespace = "com.ontrace.ontrace_flutter_plugin_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ontrace_flutter_plugin_example"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.ontrace.ontrace_flutter_plugin_example"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- ontrace_flutter_plugin/example/android/app/src/main/kotlin/com/example/ontrace_flutter_plugin_example/MainActivity.kt
+++ ontrace_flutter_plugin/example/android/app/src/main/kotlin/com/example/ontrace_flutter_plugin_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ontrace_flutter_plugin_example
+package com.ontrace.ontrace_flutter_plugin_example
 
 import io.flutter.embedding.android.FlutterActivity
 
